5bdde71f499b2876ab6d3a019480c9e3bfbf60c6
Milan Lajtoš <milan.lajtos@me.com>
2018-04-30 14:56:05 +0200

    Apostrophe-free :)

--- src/components/Interpreter/runtimeEnvironment.js
+++ src/components/Interpreter/runtimeEnvironment.js
@@ -1,89 +1,152 @@
 import * as tf from "@tensorflow/tfjs"
 import * as mnist from "mnist"
 
-const runtimeEnvironment = {
-    Tensor: tf.tensor, // noun
-    Variable: tf.variable,
-    Assign: ({ tensor, value }) => {
+class StandardLibrary {
+    Tensor = tf.tensor
+    Variable = tf.variable
+    Assign = ({ tensor, value }) => {
         tensor.assign(value)
         return tensor
-    },
-    Shape: (tensor) => tensor.shape, // noun
-    Shape: (tensor) => tf.tensor(tensor.shape), // noun
-    Rank: (tensor) => tf.scalar(tensor.rank),
-    Size: (tensor) => tf.scalar(tensor.size),
+    }
+    Shape = (tensor) => tf.tensor(tensor.shape)
+    Rank = (tensor) => tf.scalar(tensor.rank)
+    Size = (tensor) => tf.scalar(tensor.size)
 
-    Mean: tf.mean, // noun
-    Sum: ({ tensor, axis }) => tf.sum(tensor, axis),
-    Min: ({ tensor, axis }) => tf.min(tensor, axis),
+    Mean = ({ tensor, axis }) => {
+        axis = this.ConvertToNative(axis)
+        return tf.mean(tensor, axis)
+    }
+    Sum = ({ tensor, axis }) => {
+        axis = this.ConvertToNative(axis)
+        return tf.sum(tensor, axis)
+    }
+    Min = ({ tensor, axis }) => {
+        axis = this.ConvertToNative(axis)
+        return tf.min(tensor, axis)
+    }
 
-    // Shape shifters
-    Reshape: ({ tensor, shape }) => tf.reshape(tensor, shape), // noun
     /*
 
-        Reshape {shape: [100 100]} tensor
-        Reshape [100 100] tensor
-        tensor -> Reshape [100 100]
-        tensor -> Reshape { shape: [100 100] }
-        tensor.reshape([100, 100])
+        Reshape { input: tensor, shape: [100 100] }     # ukecané
+        Reshape {shape: [100 100]} tensor               # Re-shape-shape 
+        Reshape [100 100] tensor                        # hm
+        tensor -> Reshape [100 100]                     # ok
+        tensor -> Reshape { shape: [100 100] }          # ukecané
+        tensor.reshape([100, 100])                      # orig
 
     */
-    Squeeze: ({ tensor, axis }) => tf.squeeze(tensor, axis),
-    SqueezeAll: (tensor) => tf.squeeze(tensor),
-    Transpose: tf.transpose,
-    ExpandDimension: ({ tensor, axis }) => tf.expandDims(tensor, axis), // verb adverb
-    ResizeBilinear: ({ tensor, shape }) => tf.image.resizeBilinear(tensor, shape),
-    MaxPool: ({ tensor, filterSize, strides }) => tf.maxPool(tensor, filterSize, strides, "same"),
-    Tile: ({ tensor, reps }) => tf.tile(tensor, reps),
-    Slice: ({ tensor, begin, size }) => tf.slice(tensor, begin, size),
+
+    // Shape shifters
+    Reshape = ({ tensor, shape }) => {
+        shape = this.ConvertToNative(shape)
+        return tf.reshape(tensor, shape)
+    }
+    Squeeze = ({ tensor, axis }) => {
+        axis = this.ConvertToNative(axis)
+        return tf.squeeze(tensor, axis)
+    }
+    SqueezeAll = (tensor) => {
+        return tf.squeeze(tensor)
+    }
+    Transpose = (tensor) => {
+        return tf.transpose(tensor)
+    }
+    ExpandDimension = ({ tensor, axis }) => {
+        axis = this.ConvertToNative(axis)
+        return tf.expandDims(tensor, axis)
+    }
+    ResizeBilinear = ({ tensor, shape }) => {
+        shape = this.ConvertToNative(shape)
+        return tf.image.resizeBilinear(tensor, shape)
+    }
+    MaxPool = ({ tensor, filterSize, strides }) => {
+        filterSize = this.ConvertToNative(filterSize)
+        strides = this.ConvertToNative(strides)
+        return tf.maxPool(tensor, filterSize, strides, "same")
+    }
+    Tile = ({ tensor, reps }) => {
+        reps = this.ConvertToNative(reps)
+        return tf.tile(tensor, reps)
+    }
+    Slice = ({ tensor, begin, size }) => {
+        begin = this.ConvertToNative(begin)
+        size = this.ConvertToNative(size)
+        return tf.slice(tensor, begin, size)
+    }
 
     // Arithmetics
-    Negative: tf.neg, // noun OR Negate - verb
-    Add: ({a, b}) => tf.add(a, b), // verb
-    Subtract: ({a, b}) => tf.sub(a, b), // verb
-    Multiply: ({a, b}) => tf.mul(a, b), // verb
-    Divide: ({a, b}) => tf.div(a, b), // verb
-    Modulus: ({a, b}) => tf.mod(a, b), // noun
-    Power: ({a, b}) => tf.pow(a, b), // noun
-    MatrixMultiply: ({a, b}) => tf.matMul(a, b), // ?,
-    Square: tf.square,
-    SquareRoot: tf.sqrt,
-    Reciprocal: tf.reciprocal, // noun
-    Sign: tf.sign, // noun
-    Floor: tf.floor, // noun
-    Ceiling: tf.ceil, // noun
-    Round: tf.round, // verb
-    Absolute: tf.abs,
+    Negative = tf.neg
+    Add = ({a, b}) => tf.add(a, b)
+    Subtract = ({a, b}) => tf.sub(a, b)
+    Multiply = ({a, b}) => tf.mul(a, b)
+    Divide = ({a, b}) => tf.div(a, b)
+    Modulus = ({a, b}) => tf.mod(a, b)
+    Power = ({a, b}) => tf.pow(a, b)
+    MatrixMultiply = ({a, b}) => tf.matMul(a, b)
+    Square = tf.square
+    SquareRoot = tf.sqrt
+    Reciprocal = tf.reciprocal
+    Sign = tf.sign
+    Floor = tf.floor
+    Ceiling = tf.ceil
+    Round = tf.round
+    Absolute = tf.abs
 
     // Trigonometry
-    Sine: tf.sin,
-    Cosine: tf.cos,
-    Tangent: tf.tan,
-    ArcusSine: tf.asin,
-    ArcusCosine: tf.acos,
-    ArcusTangent: tf.atan,
+    Sine = tf.sin
+    Cosine = tf.cos
+    Tangent = tf.tan
+    ArcusSine = tf.asin
+    ArcusCosine = tf.acos
+    ArcusTangent = tf.atan
 
     // Activation Functions
-    ExponentialLinearUnit: tf.elu, // +-noun
-    RectifiedLinearUnit: tf.relu, // +-noun
-    Sigmoid: tf.sigmoid,
-    Softmax: tf.softmax,
-    Softplus: tf.softplus,
+    ExponentialLinearUnit = tf.elu
+    RectifiedLinearUnit = tf.relu
+    Sigmoid = tf.sigmoid
+    Softmax = tf.softmax
+    Softplus = tf.softplus
     
     // Generators
-    RandomNormal: ({ shape = [1, 1], mean = 0, stdDev = 1 }) => tf.randomNormal(shape, mean, stdDev),
-    RandomUniform: ({ shape, minval, maxval, dtype }) => tf.randomUniform(shape, minval, maxval, dtype),
-    LinearSpace: ({ start = 0, stop = 1, num = 10 }) => tf.linspace(start, stop, num),
-    Iota: (num) => tf.linspace(1, num, num),
-    Ones: ({ shape }) => tf.ones(shape),
-    Zeros: ({ shape }) => tf.zeros(shape),
+    RandomNormal = ({ shape = tf.tensor([1, 1]), mean = tf.scalar(0), stdDev = tf.scalar(1) }) => {
+        shape = this.ConvertToNative(shape)
+        mean = this.ConvertToNative(mean)
+        stdDev = this.ConvertToNative(stdDev)
+        return tf.randomNormal(shape, mean, stdDev)
+    }
+    RandomUniform = ({ shape, minval, maxval, dtype }) => {
+        shape = this.ConvertToNative(shape)
+        min = this.ConvertToNative(min)
+        max = this.ConvertToNative(max)
+        return tf.randomUniform(shape, min, max, dtype)
+    }
+    LinearSpace = ({ start = tf.scalar(0), stop = tf.scalar(1), num = tf.scalar(10) }) => {
+        start = this.ConvertToNative(start)
+        stop = this.ConvertToNative(stop)
+        num = this.ConvertToNative(num)
+        return tf.linspace(start, stop, num)
+    }
+    Iota = (value) => {
+        value = this.ConvertToNative(value)
+        return tf.linspace(1, value, value)
+    }
+    Ones = (shape) => {
+        shape = this.ConvertToNative(shape)
+        return tf.ones(shape)
+    }
+    Zeros = (shape) => {
+        shape = this.ConvertToNative(shape)
+        return tf.zeros(shape)
+    } 
 
-    GetDigit: (classes) => tf.tensor(mnist[classes].get()).reshape([28, 28]),
-    //GetDigit: (classes) => tf.randomNormal([28, 28]),
+    GetDigit = (classes = tf.scalar(0)) => {
+        classes = this.ConvertToNative(classes)
+        return tf.tensor(mnist[classes].get()).reshape([28, 28])
+    }
 
-    Gradient: tf.grad, // NOPE, not yet :D
+    Gradient = tf.grad // NOPE, not yet  =D
 
-    ConvertToNative: (tensor) => {
+    ConvertToNative = (tensor) => {
         const isTensor = (tensor instanceof tf.Tensor)
         if (!isTensor) {
             throw new Error(`Only tensors can be converted to native type.`)
@@ -111,4 +174,5 @@ const runtimeEnvironment = {
 //     })
 // }
 
-export default runtimeEnvironment
+const library = new StandardLibrary()
+export default library
