c1bcd8823875cfc443e12728601dba9132afc0a9
Milan Lajtoš <milan.lajtos@me.com>
2018-05-13 23:47:57 +0200

    Spinner is always useful, just not now

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+import "./style.sass"
+
+export default (props) => (
+    <div class="spinner center">
+        <div class="spinner-blade"></div>
+        <div class="spinner-blade"></div>
+        <div class="spinner-blade"></div>
+        <div class="spinner-blade"></div>
+        <div class="spinner-blade"></div>
+        <div class="spinner-blade"></div>
+        <div class="spinner-blade"></div>
+        <div class="spinner-blade"></div>
+        <div class="spinner-blade"></div>
+        <div class="spinner-blade"></div>
+        <div class="spinner-blade"></div>
+        <div class="spinner-blade"></div>
+    </div>
+)
+
+// https://www.cssscript.com/ios-os-x-style-pure-css-loading-spinner/
