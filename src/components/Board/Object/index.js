4b63255fd82d758aaf6f7ae9884c777724744725
Milan Lajtoš <milan.lajtos@me.com>
2018-05-18 11:58:08 +0200

    Object.betterEntries

--- src/components/Board/Object/index.js
+++ src/components/Board/Object/index.js
@@ -7,14 +7,27 @@ import "./style.sass"
 
 const _m = Symbol.for("meta")
 
+/*
+    `Object.betterEntries(obj)` returns array of triplets [value, key, obj],
+    so it is aligned with `Array.filter` and `Array.map` with signatures
+    of [elem, index, array]
+*/
+
+Object.betterEntries = (obj) => {
+    const entries = Object.entries(obj)
+    return entries.map(([key, value]) => [value, key, obj])
+}
+
+const isSilent = ([value, key, props]) => !(props[_m] && props[_m][key] && props[_m][key].silent)
+
 export default class ObjectVis extends PureComponent {
     render() {
         const { data } = this.props
 
-        const props = Object.entries(data)
-            .filter(([key]) => !data[_m][key].silent)
-            .map(([key, value]) => {
-                const _meta = data[_m][key]
+        const props = Object.betterEntries(data)
+            .filter(isSilent)
+            .map(([value, key, props]) => {
+                const _meta = props[_m][key]
 
                 return (
                     <ObjectProperty {...{
