6a9262504948ab7ebdee74c5e9a7a26137ea2b8d
Milan Lajtoš <milan.lajtos@me.com>
2018-05-22 10:45:27 +0200

    Perfection^2

--- src/components/MonacoEditor/index.js
+++ src/components/MonacoEditor/index.js
@@ -115,7 +115,7 @@ const provider = {
         root: [
             [/-?[0-9][0-9_]*(\.[0-9_]*)?/, "number"],
             [/\p{Lu}(\p{Ll}|\p{Lu}|\p{Lt}|\p{Lm}|\p{Lo}|[-_0-9])*/u, "type.identifier"],
-            [/(\p{Ll}|\p{Lu}|\p{Lt}|\p{Lm}|\p{Lo}|[-_0-9])+/u, "identifier"],
+            [/(\p{Ll}|\p{Lu}|\p{Lt}|\p{Lm}|\p{Lo})(\p{Ll}|\p{Lu}|\p{Lt}|\p{Lm}|\p{Lo}|[-_0-9])*/u, "identifier"],
             [/(=>|->|:|\+|-|\*|\/|\^|@|%|\.|×|@|%|÷|∇)/, "operator"],
             [/[{}()\[\]]/, "@brackets"],
             [/[;|].*$/, "comment"]
