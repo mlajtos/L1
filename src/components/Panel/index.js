f8877b9aacf231435e3e5b74df679eee79215e01
Milan Lajtoš <milan.lajtos@me.com>
2018-05-23 10:04:02 +0200

    Scroll decoration

--- src/components/Panel/index.js
+++ src/components/Panel/index.js
@@ -3,10 +3,28 @@ import React, { PureComponent } from "react"
 import "./style.sass"
 
 export default class Panel extends PureComponent {
+    state = {
+        scrolled: false
+    }
+    onScroll = (e) => {
+        if (e.target.scrollTop > 0 && !this.state.scrolled) {
+            this.setState({
+                scrolled: true
+            })
+            return
+        }
+
+        if (e.target.scrollTop === 0 && this.state.scrolled) {
+            this.setState({
+                scrolled: false
+            })
+            return
+        }
+    }
     render() {
 
         return (
-            <div className={"panel"}>
+            <div className={"panel " + (this.state.scrolled ? "scrolled" : "")} onScroll={this.onScroll}>
                 { this.props.children }
             </div>
         )
