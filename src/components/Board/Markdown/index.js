ad82eb889c5ab3e3d6c1d50928043be664d1ae9a
Milan Lajtoš <milan.lajtos@me.com>
2018-06-09 14:22:19 +0200

    Markdown for docs

---
+++ src/components/Board/Markdown/index.js
@@ -0,0 +1,64 @@
+import React, { PureComponent } from "react"
+
+import monaco, { renderMarkdown } from "../../MonacoEditor"
+
+import { MarkdownRenderer } from 'monaco-editor/esm/vs/editor/contrib/markdown/markdownRenderer';
+import { StaticServices } from 'monaco-editor/esm/vs/editor/standalone/browser/standaloneServices';
+
+console.log(StaticServices)
+const renderer = new MarkdownRenderer(monaco.editor.create(document.createElement("div")), StaticServices.modeService.get())
+const el = renderer.render({
+    value: "# L1\n```L1\na: 23```"
+}).element
+
+console.log(el, el.innerHTML)
+
+import "./style.sass"
+
+const markdownToHTML = (value) => {
+    const result = renderMarkdown({
+        value
+    }).innerHTML
+    return result
+}
+
+const markdownToHTML_2 = (value) => {
+    const result = renderer.render({
+        value
+    }).element.innerHTML
+    return result
+}
+
+export default class Markdown extends PureComponent {
+    state = {
+        colorizedValue: null,
+        mounted: false
+    }
+    colorize = async (value) => {
+        const stringValue = "" + value
+        const colorizedValue = markdownToHTML(stringValue)
+        // const colorizedValue = 
+        // console.log(colorizedValue)
+        if (this._mounted) {
+            this.setState({ colorizedValue })
+        }
+    }
+    componentDidUpdate() {
+        this.colorize(this.props.children)
+    }
+    componentDidMount() {
+        this._mounted = true
+        this.colorize(this.props.children)
+    }
+    componentWillUnmount() {
+        this._mounted = false
+    }
+    render() {
+        if (!this.state.colorizedValue) {
+            return <div className="property markdown">{this.props.children}</div>
+        } else {
+            return <div className="property markdown" dangerouslySetInnerHTML={{__html: this.state.colorizedValue}} />
+        }
+    }
+
+}
