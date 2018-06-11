0bffe5ab488819997cb960c4698ad10dfe88af91
Milan Lajtoš <milan.lajtos@me.com>
2018-06-11 23:41:09 +0200

    Expand vis selection

--- src/components/Board/ObjectProperty/index.js
+++ src/components/Board/ObjectProperty/index.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from "react"
 import * as tf from "@tensorflow/tfjs-core"
-import { isObject, isFunction, stubTrue } from "lodash-es"
+import { isObject, isFunction, stubTrue, isString, isNumber, isBoolean } from "lodash-es"
 import { Observable } from "rxjs"
 
 import TensorVis from "../Tensor"
@@ -12,17 +12,51 @@ import ObservableVis from "../Observable"
 import ObjectVis from "../Object"
 import ErrorVis from "../Error"
 
-const isUndefined = (value) => !value
+import PropertyWrapper from "../PropertyWrapper"
+
+const isUndefined = (value) => (value === undefined)
+const isNull = (value) => (value === null)
 const isPromise = (value) => (value.toString() === "[object Promise]")
 const isTensor = (value) => value instanceof tf.Tensor
 const isScope = (value) => (isObject(value) && !isPromise(value))
 const isError = (value) => value instanceof Error
 const isObservable = (value) => value instanceof Observable
 
+const StringVis = (props) => (
+    <PropertyWrapper {...props} type="string" symbol="„abc“">
+        <div>{props.data}</div>
+    </PropertyWrapper>
+)
+
+const NumberVis = (props) => (
+    <PropertyWrapper {...props} type="number" symbol="123">
+        <div>{props.data}</div>
+    </PropertyWrapper>
+)
+
+const BooleanVis = (props) => (
+    <PropertyWrapper {...props} type="boolean" symbol="0/1">
+        <div>{props.data ? "true" : "false"}</div>
+    </PropertyWrapper>
+)
+
+const UndefinedVis = (props) => (
+    <PropertyWrapper {...props} type="undefined" symbol="?">
+    </PropertyWrapper>
+)
+
+const NullVis = (props) => (
+    <PropertyWrapper {...props} type="null" symbol="NULL" />
+)
+
 export default class ObjectProperty extends PureComponent {
     visualizations = [
-        [isUndefined,   UnknownVis    ],
+        [isUndefined,   UndefinedVis  ],
+        [isNull,        NullVis       ],
+        [isBoolean,     BooleanVis    ],
         [isError,       ErrorVis      ],
+        [isString,      StringVis     ],
+        [isNumber,      NumberVis     ],
         [isTensor,      TensorVis     ],
         [isFunction,    FunctionVis   ],
         [isObservable,  ObservableVis ],
