b2f86fe13b5bfe3688aa8d5556b87b339c613990
Milan Lajtoš <milan.lajtos@me.com>
2018-06-21 15:13:35 +0200

    Not needed

--- src/components/Evaluator/index.js
+++ src/components/Evaluator/index.js
@@ -1,12 +1,10 @@
 import Parser from "../Parser"
 import Interpreter from "../Interpreter"
-import { Subject } from "rxjs"
 
 class Evaluator {
     evaluate = async (code, env = {}, issues) => {
         const parsingResult = await Parser.parse(code, issues)
         const ast = parsingResult.result || null
-        const parsingError =  parsingResult.error || null
         const interpretingResult = ast ? await Interpreter.interpret(ast, env, issues) : null
         const computedValues = interpretingResult ? interpretingResult.success.result || [] : null
 
