5ef4e62c209fcb1dc0fe6c6b8487dba4b129a1c5
Milan Lajtoš <milan.lajtos@me.com>
2018-07-06 13:23:42 +0200

    Normal quotes

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import Studio from "./components/Studio"
 
 self.MonacoEnvironment = {
     getWorkerUrl: function (moduleId, label) {
-        if (label === 'json') {
-            return './json.worker.js';
+        if (label === "json") {
+            return "./json.worker.js";
         }
-        // if (label === 'css') {
-        // 	return './css.worker.js';
+        // if (label === "css") {
+        // 	return "./css.worker.js";
         // }
-        // if (label === 'html') {
-        // 	return './html.worker.js';
+        // if (label === "html") {
+        // 	return "./html.worker.js";
         // }
-        // if (label === 'typescript' || label === 'javascript') {
-        // 	return './ts.worker.js';
+        // if (label === "typescript" || label === "javascript") {
+        // 	return "./ts.worker.js";
         // }
-        return './editor.worker.js';
+        return "./editor.worker.js";
     }
 }
 
